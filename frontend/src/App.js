caacb35f38e441b8f846fd93b016ed6110d0c12e
sambattis <123843507+sambattis@users.noreply.github.com>
2023-11-21 17:00:26 -0500

    Map test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
 
-function App() {
+//import logo from './logo.svg';
+//import './App.css';
+
+import {useState} from "react";
+
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps"
+
+
+export default function App() {
+  const position = {lat: 53.54, lng: 10};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h>NuCasa</h>
-      </header>
-    </div>
+    <APIProvider apiKey = {process.env.GOOGLE_MAPS_API_KEY}>
+      <div className="App">
+        <header className="App-header">
+          <h>NuCasa</h>
+        </header>
+      </div>
+      
+      <div style = {{height: "97vh"}}>
+        <Map zoom = {9} center = {position}></Map>
+      </div>
+    </APIProvider>
   );
 }
 
-export default App;
+//export default App;
+
