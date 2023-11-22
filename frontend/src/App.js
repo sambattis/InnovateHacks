3db01768df9e9d4452ba4fdd45174e088d7f5527
sambattis <123843507+sambattis@users.noreply.github.com>
2023-11-21 20:04:37 -0500

    struggling to implement directions finding service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,50 @@
 //import logo from './logo.svg';
 //import './App.css';
 
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 import {
   APIProvider,
+  useDirectionService,
   Map,
   AdvancedMarker,
   Pin,
   InfoWindow,
+  AutocompleteProps,
+  DirectionsRenderer,
 } from "@vis.gl/react-google-maps"
 
 
 export default function App() {
+
   const position = {lat: 53.54, lng: 10};
+
+  //feel free to get rid of as much of this implementation as necessary, it wasn't working out because google.map.DirectionService() was not working.
+  //"google" was undefined. Not sure if this is because immediately above it, there was no API importing thing, whereas later on when "Map" is used,
+  //it is wrapped in <APIProvider></APIProvider
+
+  const [map, setMap] = useState((null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [travelTime, setTravelTime] = useState('')
+  const [distance, setDistance] = useState('')
+  const destinationRef = useRef()
+  const originRef = useRef()
+  const travelMethodRef = useRef()
+
+  async function findRoute() {
+    if (destinationRef.current.value == '' || originRef.current.value == '') {
+      return;
+    } 
+
+    //const dService = new google.map.DirectionService()
+    /*
+    const results = await dService.route({
+      travelMode: travelMethodRef.current.value,
+      destination: destinationRef.current.value,
+      origin: originRef.current.value,
+    })
+    */
+  }
 
   return (
     <APIProvider apiKey = {process.env.GOOGLE_MAPS_API_KEY}>
@@ -31,6 +62,4 @@ export default function App() {
     </APIProvider>
   );
 }
-
-//export default App;
 
