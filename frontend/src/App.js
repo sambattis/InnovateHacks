125f8cf3f87fc8a31f37a60f60de76bd89fb245d
sambattis <123843507+sambattis@users.noreply.github.com>
2024-08-25 18:25:07 -0400

    kinda a mess

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, useEffect} from "react";
 //import * as React from 'react';
 import {
   APIProvider,
@@ -11,27 +11,17 @@ import {
   Map,
 } from "@vis.gl/react-google-maps"
 
-import Multiple from "./components/Form.js";
+import Form from "./components/Form.js";
 
 const google = window.google
 
 export default function App() {
 
-  //const [selected, setSelected] = useState(null);
-  // const position = {lat: 51, lng: 9}; 
-  // const [data, setData] = useState(10); passes all data from from to map
   const [position, setPosition] = useState({lat: 51, lng: 10});
   const [data, setData] = useState({car: "", walk: "", bus: "", coX: "", coY: "", coX1: "", coY1: "", coX2: "", coY2: ""});
 
-  const childToParent = (childdata) => {
-    setData(childdata);
-    findRouteHelper(data);
-  }
-
   const [map, setMap] = useState((null))
   const [dResponse, setDResponse] = useState(null)
-  const [travelTime, setTravelTime] = useState('')
-  const [distance, setDistance] = useState('')
   const [drivingPref, setDrivingPref] = useState('')
   const [bikePref, setBikePref] = useState('')
   const [walkPref, setWalkPref] = useState('')
@@ -57,28 +47,48 @@ export default function App() {
 
   const [bestX, setBestX] = useState(0)
   const [bestY, setBestY] = useState(0)
-  const key = "AIzaSyDqxDeOp_BmTjMT9xAeqYa2WWamyMPLjYk";
+  const key = process.env.REACT_APP_API_KEY;
   console.log(key);
 
-  return (
-    
-    <APIProvider apiKey = {key} >
+  useEffect (() => {
+    console.log('You should live at');
+    console.log(bestX);
+    console.log(bestY);
+    console.log({lat: bestX, lng:bestY});
+    setPosition({lat: bestX, lng:bestY});
+  }, [bestY])
 
+  useEffect (() => {
+    console.log(data);
+   if (data.coX) {
+     console.log('true');
+     setBikePref(data.bike);
+     setWalkPref(data.walk);
+     setDrivingPref(data.car);
+     setTransitPref(data.transit);
+     setPlaceOne(Place(data.coX, data.coY, data.freq));
+     setPlaceTwo(Place(data.coX1, data.coY1, data.freq1));
+     setPlaceThree(Place(data.coX2, data.coY2, data.freq2));
+     setPlaceFour(Place(data.coX3, data.coY3, data.freq3));
+     setPlaceFive(Place(data.coX4, data.coY4, data.freq4));
+   }
+ }, [data])
+
+ useEffect (() => {
+   startCalcs(data);
+ }, [PlaceFive])
+
+  return (
+    <APIProvider apiKey = {key} >
       <div className="App">
         <header className="App-header">
           <h3> <i class="fas fa-home"></i> NuCasa <i class="fas fa-map-marker-alt"></i></h3>
         <i className="small-text" >The perfect app for quick location triangulation! </i> 
-
         </header>
       </div>
-     
       <div className="split left-panel ">
-        <Multiple childToParent={childToParent}/>
-          <p>{childToParent}</p>
-          <button className = "hide" primary onClick={() => childToParent(data)}>Click Child</button> 
+        <Form data={data} setData={setData}/>
       </div>
-
-
       <div className="split right-panel " style = {{height: "95vh"} }>
         <Map zoom = {9} center = {position} onLoad={map => setMap(map)}>
         {dResponse && (
@@ -90,48 +100,64 @@ export default function App() {
     </APIProvider>
     
   );
-//onLoad={map => setMap(map)}
-//need to hide the childtoparent button
 
+  async function startCalcs(data) {
+    let minX = 90;
+    let maxX = -90;
+    let minY = 90;
+    let maxY = -90;
+    
+    let newList = [];
+    if (PlaceOne.xCo_ != "" && PlaceOne.yCo_ != "" && PlaceOne.freq_ != "") {
+      console.log('reached One');
+      newList = newList.concat(PlaceOne);
+    }
 
+    if (PlaceTwo.xCo_ !== "" && PlaceTwo.yCo_ !== "" && PlaceOne.freq_ != "") {
+      console.log('reached Two');
+      newList = newList.concat(PlaceTwo);
+    }
+    if (PlaceThree.xCo_ !== "" && PlaceThree.yCo_ !== "" && PlaceThree.freq_ != "") {
+      console.log('reached Three');
+      newList = newList.concat(PlaceThree);
+    }
 
-async function calculateStrength(xCo, yCo) {
-  let totalScore = 0;
-  //console.log(xCo);
-  let dScore = 99999;
-  let bScore = 99999;
-  let wScore = 99999;
-  //console.log(yCo);
-  for (let i = 0; i < list.length; i++) {
-    //console.log((await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'DRIVING')));
-    dScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'DRIVING')) * parseFloat(drivingPref);
-    bScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'BICYCLING')) * parseFloat(bikePref);
-    wScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'WALKING')) * parseFloat(walkPref);
-    //setTScore(findRoute(xCo, yCo, PlaceOne.xCo_,PlaceOne.yCo_, 'TRANSIT') * transitPref);
-    //console.log(parseFloat(dScore));
-    //console.log(bScore);
-    //console.log(wScore);
-    let bestScore = 0;
-    if (dScore < bScore && dScore < wScore) {
-      bestScore = dScore;
+    if (PlaceFour.xCo_ !== "" && PlaceFour.yCo_ !== "" && PlaceFour.freq_ != "") {
+      console.log('reached Four');
+      newList = newList.concat(PlaceFour);
     }
-    if (bScore < dScore && bScore < wScore) {
-      bestScore = bScore;
+
+    if (PlaceFive.xCo_ != "" && PlaceFive.yCo_ != "" && PlaceFive.freq_ != "") {
+      console.log('reached Five');
+      newList = newList.concat(PlaceFive);
     }
-    if (wScore < bScore && wScore < dScore) {
-      bestScore = wScore;
+    
+    const finalNewList = newList;
+    setList(finalNewList);
+
+    console.log(list);
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].xCo_ > maxX) {
+        maxX = list[i].xCo_;
+      }
+      if (list[i].xCo_ < minX) {
+        minX = list[i].xCo_;
+      }
+      if (list[i].yCo_ > maxY) {
+        maxY = list[i].yCo_;
+      }
+      if (list[i].yCo_ < minY) {
+         minY = list[i].yCo_;
+      }
     }
-    //console.log(bestScore);
-    let curScore = parseFloat(bestScore) * parseFloat(list[i].freq_);
-    //console.log(totalScore);
-   // console.log(curScore);
-    //console.log(parseFloat(curScore));
-    totalScore += parseFloat(curScore);
-    //console.log(totalScore);
+    console.log(maxX);
+    console.log(maxY);
+    console.log(minX);
+    console.log(minY);
+
+    await findBestHome(minX, maxX, minY, maxY);
+    
   }
-  //console.log(totalScore);
- return totalScore;
-}
     
     async function findBestHome(minX, maxX, minY, maxY) {
       let bestScore = 99999999;
@@ -189,74 +215,37 @@ async function calculateStrength(xCo, yCo) {
       setBestY(parseFloat(firstBestY));
     }
 
-    async function findRouteHelper(data) {
-      let minX = 90;
-      let maxX = -90;
-      let minY = 90;
-      let maxY = -90;
-      setBikePref(data.bike);
-      setWalkPref(data.walk);
-      setDrivingPref(data.car);
-      setTransitPref(data.transit);
-      setPlaceOne(Place(data.coX, data.coY, data.freq));
-      setPlaceTwo(Place(data.coX1, data.coY1, data.freq1));
-      setPlaceThree(Place(data.coX2, data.coY2, data.freq2));
-      setPlaceFour(Place(data.coX3, data.coY3, data.freq3));
-      setPlaceFive(Place(data.coX4, data.coY4, data.freq4));
-      let newList = [];
-      if (PlaceOne.xCo_ != "" && PlaceOne.yCo_ != "" && PlaceOne.freq_ != "") {
-        console.log('reached One');
-        newList = newList.concat(PlaceOne);
-      }
-
-      if (PlaceTwo.xCo_ !== "" && PlaceTwo.yCo_ !== "" && PlaceOne.freq_ != "") {
-        console.log('reached Two');
-        newList = newList.concat(PlaceTwo);
-      }
-      if (PlaceThree.xCo_ !== "" && PlaceThree.yCo_ !== "" && PlaceThree.freq_ != "") {
-        console.log('reached Three');
-        newList = newList.concat(PlaceThree);
-      }
-
-      if (PlaceFour.xCo_ !== "" && PlaceFour.yCo_ !== "" && PlaceFour.freq_ != "") {
-        console.log('reached Four');
-        newList = newList.concat(PlaceFour);
-      }
-
-      if (PlaceFive.xCo_ != "" && PlaceFive.yCo_ != "" && PlaceFive.freq_ != "") {
-        console.log('reached Five');
-        newList = newList.concat(PlaceFive);
-      }
-      
-      const finalNewList = newList;
-      setList(finalNewList);
-
-      console.log(list);
+    async function calculateStrength(xCo, yCo) {
+      let totalScore = 0;
+      //console.log(xCo);
+      let dScore = 99999;
+      let bScore = 99999;
+      let wScore = 99999;
+      //console.log(yCo);
       for (let i = 0; i < list.length; i++) {
-        if (list[i].xCo_ > maxX) {
-          maxX = list[i].xCo_;
+        //console.log((await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'DRIVING')));
+        dScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'DRIVING')) * parseFloat(drivingPref);
+        bScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'BICYCLING')) * parseFloat(bikePref);
+        wScore = parseFloat(await findRoute(xCo, yCo, list[i].xCo_,list[i].yCo_, 'WALKING')) * parseFloat(walkPref);
+        //setTScore(findRoute(xCo, yCo, PlaceOne.xCo_,PlaceOne.yCo_, 'TRANSIT') * transitPref);
+        //console.log(parseFloat(dScore));
+        //console.log(bScore);
+        //console.log(wScore);
+        let bestScore = 0;
+        if (dScore < bScore && dScore < wScore) {
+          bestScore = dScore;
         }
-        if (list[i].xCo_ < minX) {
-          minX = list[i].xCo_;
+        if (bScore < dScore && bScore < wScore) {
+          bestScore = bScore;
         }
-        if (list[i].yCo_ > maxY) {
-          maxY = list[i].yCo_;
+        if (wScore < bScore && wScore < dScore) {
+          bestScore = wScore;
         }
-        if (list[i].yCo_ < minY) {
-           minY = list[i].yCo_;
-        }
+        let curScore = parseFloat(bestScore) * parseFloat(list[i].freq_);
+        totalScore += parseFloat(curScore);
+        //console.log(totalScore);
       }
-      console.log(maxX);
-      console.log(maxY);
-      console.log(minX);
-      console.log(minY);
-
-      await findBestHome(minX, maxX, minY, maxY);
-      console.log('You should live at');
-      console.log(bestX);
-      console.log(bestY);
-      console.log({lat: bestX, lng:bestY});
-      setPosition({lat: bestX, lng:bestY});
+     return totalScore;
     }
 
   function parseTime(inputString) {
@@ -341,8 +330,8 @@ async function calculateStrength(xCo, yCo) {
       travelMode: method,
     })
 
-    setDistance(result.routes[0].legs[0].distance.text)
-    setTravelTime(result.routes[0].legs[0].duration.text)
+    //setDistance(result.routes[0].legs[0].distance.text)
+    //setTravelTime(result.routes[0].legs[0].duration.text)
     //parse time string and turn into double
     let time = parseTime(result.routes[0].legs[0].duration.text)
     // /console.log(time)
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -21,15 +21,13 @@ const google = window.google
 
 //Worst case handle api with form and save ideal location locally! & pull it down from local storage 
 
-export default function Multiple({childToParent}){
+export default function Form({data, setData}){
   const [formData, setFormData] = useState({car: "",walk: "",bike: "",coX: "",coY: "",freq: "",coX1: "",coY1: "", freq1: "", coX2: "", coY2: "", freq2: "", coX3: "", coY3: "", freq3: "", coX4: "", coY4: "", freq4: ""});
   //const [formData, setFormData] = useState({car: "",walk: "",transit: "",bike: "",coX: "",coY: "",coX1: "",coY1: "",coX2: "", coY2: ""});
   const [LatLng, setLatLng] = useState();
   const [address, setAddress] = useState();
 
 //   const data = "This is data from Child Component to the Parent Component."
-
-  const [data, setData] = useState(formData);
 
 
   const handleChange = (event) => {
@@ -39,8 +37,8 @@ export default function Multiple({childToParent}){
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(formData.coX1, formData.coY1);
     setData(formData);
-    childToParent(data);
 };
 
 async function findLocation(data) {
@@ -184,7 +182,6 @@ return (
       <button  className="button-4"    onClick ={() =>algorithmExp(data)} type="button" class="icon-button" aria-label="Help">
       How does NuCasa Work? <i class="fas fa-cogs"></i>
    </button>
-      {/* <button primary onClick={() => childToParent(data)}>Click Child</button> */}
 
     </form>
     </div>
